fc035b1bc3f0d7afb790ff7c03fdd068299c5d3c
Amanda <amandaqcarreiro@gmail.com>
2022-11-13 20:35:03 -0300

    Adicionando tabelas ao banco

--- PDW-projeto/database/migrations/2022_11_13_224707_ingredients_table.php
+++ PDW-projeto/database/migrations/2022_11_13_224707_ingredients_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('ingredients', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestpphamps();
+            $table->string('nome');
+            $table->integer('qntd');
+            $table->string('tipo_qntd');
+            $table->string('tipo_ingredients');
         });
     }
 
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('ingredients');
     }
 };
