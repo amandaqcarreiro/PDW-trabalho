bb3ca4f5960555baea1aca581ccac5b3d1b5c2eb
VinnyDSR <r.viniciusds@gmail.com>
2022-11-13 03:03:53 -0300

    primeira parte do front

---
+++ PDW-projeto/resources/views/calendar.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="{{ asset('assets/css/reset.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/css/calendar.css') }}">
+	<title>Calendário</title>
+</head>
+
+<body>
+	<div class="header__balls">
+		<div class="balls_container">
+			<div class="balls"></div>
+			<div class="balls"></div>
+			<div class="balls"></div>
+		</div>
+	</div>
+	<header>
+		<div></div>
+		<h1 class="header__text">Meu almoço</h1>
+		<img class="header__img" src="{{ asset('assets/img/boneco.png') }}" alt="Foto de um boneco">
+	</header>
+	<main class="main-content">
+		<div class="main__header">
+			<div></div>
+			<p class="day food_name">02 - 08 / 11</p>
+			<img src="{{ asset('assets/img/pencil.png') }}"  alt="pencil" class="main__header-img">
+		</div>
+		<div class="food_element">
+			<div class="food_number">02</div>
+			<p class="food_name">Strogonoff de frango</p>
+		</div>
+		<div class="food_element">
+			<div class="food_number">03</div>
+			<p class="food_name">Steak au poivre</p>
+		</div>
+		<div class="food_element">
+			<div class="food_number">04</div>
+		</div>
+		<div class="food_element">
+			<div class="food_number">05</div>
+		</div>
+		<div class="food_element">
+			<div class="food_number">06</div>
+		</div>
+	</main>
+</body>
+
+</html>
---
+++ PDW-projeto/resources/views/create_user.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="{{ asset('assets/css/reset.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/css/login.css') }}">
+	<title>Meu almoço</title>
+</head>
+
+<body>
+	<header class="header__balls">
+		<div class="balls_container">
+			<div class="balls"></div>
+			<div class="balls"></div>
+			<div class="balls"></div>
+		</div>
+	</header>
+	<main class="main-content">
+		<form class="form" action="">
+			<h1 class="main-title">Meu Almoço</h1>
+			<input type="text" class="input" placeholder="Nome de usuário"></input>
+			<input type="email" class="input" placeholder="Email"></input>
+			<input type="password" class="input" placeholder="Senha"></input>
+			<input type="password" class="input" placeholder="Confirme sua senha"></input>
+			<button type="submit" href="" class="main__botao-submit">Enviar</button>
+		</form>
+
+	</main>
+</body>
+
+</html>
